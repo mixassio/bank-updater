5bbfeec95d7d3faca8c3c6d387aeea98082a0d37
mixassio <mixail.bondarev@gmail.com>
2018-12-05 15:17:18 +0300

    add load and parsing  XML.

--- src/app.js
+++ src/app.js
@@ -4,18 +4,39 @@ import _ from 'lodash';
 export default () => {
   const state = {};
 
-  const input = document.getElementById('exampleFormControlFile1');
+  const inputBD = document.getElementById('fileBD');
+  const inputXML = document.getElementById('XMLcbrf');
   const form = document.querySelector('form');
   form.addEventListener('submit', async (e) => {
-    const fReader = new FileReader();
-    await fReader.readAsText(input.files[0]);
-    fReader.onloadend = (event) => {
+    const fReaderCSV = new FileReader();
+    await fReaderCSV.readAsText(inputBD.files[0]);
+    fReaderCSV.onloadend = (event) => {
       const dataParsed = Papa.parse(event.target.result, { encoding: 'utf-8' });
-      console.log(dataParsed.data);
+      // console.log(dataParsed.data);
       const { data: [keys, ...values] } = dataParsed;
-      state.currentBanks = values.map(bank => _.zipObject(keys, bank));
-      console.log(state);
+      state.currentBanksBD = values.map(bank => _.zipObject(keys, bank));
+    };
+    const fReaderXML = new FileReader();
+    await fReaderXML.readAsText(inputXML.files[0], 'CP1251');
+    fReaderXML.onloadend = (event2) => {
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(event2.target.result, 'application/xml');
+      // console.log(doc);
+      const items = doc.querySelectorAll('BICDirectoryEntry');
+      state.new = [...items].map((item) => {
+        const info = item.querySelector('ParticipantInfo');
+        const bankInfo = [...info.attributes].reduce((acc, el) => (
+          { ...acc, [el.name]: el.textContent }
+        ), {});
+        const accountsList = item.querySelectorAll('Accounts');
+        const accounts = [...accountsList]
+          .map(account => [...account.attributes].reduce((acc, el) => (
+            { ...acc, [el.name]: el.textContent }
+          ), {}));
+        return { ...bankInfo, accounts, bic: item.getAttribute('BIC') };
+      });
     };
     e.preventDefault();
   });
+  console.log(state);
 };
